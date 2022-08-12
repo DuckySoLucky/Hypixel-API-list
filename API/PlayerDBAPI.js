9cbdb155a21b0d8565a004b7d666878bc7b8a28a
DuckySoLucky <robertkovac160@gmail.com>
2022-08-12 19:18:13 +0200

    Update

--- API/PlayerDBAPI.js
+++ API/PlayerDBAPI.js
@@ -1,10 +1,20 @@
 const axios = require('axios')
 const config = require('../config.json')
 
-async function getPlayerData(username) {
-    const response = await axios.get(`${config.api.playerDBAPI}/${username}`)
-    if(response.data.code != 'player.found'){return 'There is no player with the given UUD or name.'}
-    return response.data.data.player.raw_id
+async function getUUID(username) {
+    try {
+        return (await axios.get(`${config.api.playerDBAPI}/${username}`)).data.data.player.raw_id
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-module.exports = {getPlayerData};
+async function getUsername(uuid) {
+    try {
+        return (await axios.get(`${config.api.playerDBAPI}/${uuid}`)).data.data.player.username
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { getUUID, getUsername };
--- API/SkyHelperAPI.js
+++ API/SkyHelperAPI.js
@@ -1,33 +1,24 @@
 const axios = require('axios')
 const config = require('../../../config.json')
 
+async function getProfiles(username) {
+    try {
+        return (await axios.get(`${config.api.skyHelperAPI}/profiles/${username}?key=${config.api.skyHelperKey}`)).data
+    } catch (error) {
+        return error
+    }
+}
+
 async function getProfile(username, profile) {
     try {
-        const response = await axios.get(`${config.api.skyHelperAPI}/profiles/${username}?key=${config.api.skyHelperKey}`)
-        if(response.data.status == 404){return 'ERROR There is no player with the given UUID or name or the player has no Skyblock profiles'}
-        if (profile != undefined) {
-            for (let i = 0; i < Object.keys(response.data.data).length; i++) {
-                if (response.data.data[i].name == profile) {
-                    return response.data.data[i]
-                } else {
-                    if (i+1 >= Object.keys(response.data.data).length) {
-                        return 'ERROR There is no player with the given UUID or name or the player has no Skyblock profiles'
-                    }
-                }
-            }
-        } else { 
-            return response.data.data[0]
-        }
+        return (await axios.get(`${config.api.skyHelperAPI}/profile/${username}/${profile}?key=${config.api.skyHelperKey}`)).data
     } catch (error) {
-        return `ERROR ` + error
+        return error
     }
-
 }
 
 async function getFetchur() {
-    const response = await axios.get(`${config.api.skyHelperAPI}/fetchur?key=${config.api.skyHelperKey}`)
-    if(response.status == 404){return 'Something went wrong, try again later.'}
-    return response.data
+    return await axios.get(`${config.api.skyHelperAPI}/fetchur?key=${config.api.skyHelperKey}`).data
 }
 
-module.exports = { getFetchur, getProfile }
+module.exports = { getFetchur, getProfiles, getProfile }
